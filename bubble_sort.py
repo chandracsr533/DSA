155b4663bed72812b7640f5550ddcac74819a4d1
chandracsr533 <csrchandra533@gmail.com>
2026-09-15 20:06:09 +0530

    bubble sort problem

---
+++ bubble_sort.py
@@ -0,0 +1,10 @@
+numbers = [9, 4, 7, 2, 6, 1]
+
+n = len(numbers)
+
+for i in range(n):
+    for j in range(0,n-i-1):
+        if numbers[j]>numbers[j+1]:
+            numbers[j],numbers[j+1] = numbers[j+1],numbers[j]
+
+print(numbers)
