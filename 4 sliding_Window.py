4fc7fc45f9345fc134ee6d7d69f2f50c777e2efb
chandracsr533 <csrchandra533@gmail.com>
2026-09-20 21:37:03 +0530

    Sliding window program added

---
+++ 4 sliding_Window.py
@@ -0,0 +1,32 @@
+numbers = [2, 4, 6, 1, 3, 5]
+k = 2
+n = len(numbers)
+
+window_sum = sum(numbers[:k])
+maximum = window_sum
+
+for i in range(k, n):
+    window_sum = window_sum - numbers[i-k] + numbers[i]
+
+    maximum = max(maximum, window_sum)
+
+print(maximum)
+
+
+############
+
+
+# numbers = [4, 2, 1, 7, 8, 1, 2]
+# k = 3
+
+# n = len(numbers)
+# maximum = 0 
+
+# for i in range(0, n-k+1):
+#     current_sum = 0
+#     for j in range(i, i+k):
+#         current_sum += numbers[j]
+#     if current_sum > maximum:
+#         maximum = current_sum
+
+# print(maximum)
