b41822af0ca788e702e424dd0eb0cab735b20ec8
chandracsr533 <csrchandra533@gmail.com>
2026-09-26 07:43:39 +0530

    Modified practice file

---
+++ practice.py
@@ -0,0 +1,17 @@
+# # numbers = [4, 2, 1, 7, 8, 1, 2]
+# # k = 3
+
+# numbers = [2, 4, 6, 1, 3, 5]
+# k = 2
+
+# n = len(numbers)
+# maximum = 0
+
+# for i in range(0,n-k+1):
+#     current_sum = 0
+#     for j in range(i,i+k):
+#         current_sum += numbers[j]
+#     if current_sum > maximum:
+#         maximum = current_sum
+# print(maximum)
+
